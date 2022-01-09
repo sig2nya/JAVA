ade016d0b60f807b6a905ae471ec9a82ddb1e42f
sig2nya <starwalk7058@naver.com>
2022-01-09 14:06:43 +0900

    How to do Test

---
+++ src/main/java/hello/hellospring/service/MemberService.java
@@ -0,0 +1,35 @@
+package hello.hellospring.service;
+
+import hello.hellospring.domain.Member;
+import hello.hellospring.repository.MemberRepository;
+import hello.hellospring.repository.MemoryMemberRepository;
+
+import java.util.List;
+import java.util.Optional;
+
+public class MemberService {
+    private final MemberRepository memberRepository = new MemoryMemberRepository();
+    public Long join(Member member){
+        /*
+        회원 가입
+         */
+        validateDuplicateMember(member);
+        memberRepository.save(member);
+        return member.getId();
+    }
+
+    private void validateDuplicateMember(Member member) {
+        memberRepository.findByName(member.getName())
+            .ifPresent(m->{ // 중복된 이름 회원 X
+            throw new IllegalStateException("이미 존재하는 회원입니다.");
+            });
+    }
+
+    public List<Member> findMembers(){
+        return memberRepository.findAll();
+    }
+
+    public Optional<Member> findOne(Long memberId){
+        return memberRepository.findById(memberId);
+    }
+}
