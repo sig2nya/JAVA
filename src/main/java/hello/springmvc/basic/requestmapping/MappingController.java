2a329a9a47d60aa0775d7946b07e29e67bd0b433
sig2nya <starwalk7058@naver.com>
2022-02-02 17:49:20 +0900

    RequestMapping

---
+++ src/main/java/hello/springmvc/basic/requestmapping/MappingController.java
@@ -0,0 +1,58 @@
+package hello.springmvc.basic.requestmapping;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+public class MappingController {
+    private Logger log = LoggerFactory.getLogger(getClass());
+
+    @RequestMapping({"/hello-basic","/hello-go"}) // 여러 url을 지정도 가능
+    public String helloBasic(){
+        log.info("helloBasic");
+        return "ok";
+    }
+
+    @GetMapping("/mapping-get-v2")
+    public String mappingGetV2(){
+        log.info("mapping-get-ve");
+        return "ok";
+    }
+
+    @GetMapping("/mapping/{userId}") // Path Variable을 이용한 매핑, gitHub에서도 이 방식 사용했던 것 같음
+    public String mappingPath(@PathVariable("userId") String userId){
+        log.info("mappingPath userId = {}", userId);
+        return "ok";
+    }
+
+    @GetMapping("/mapping/users/{userId}/orders/{orderId}")
+    public String mappingPath(@PathVariable String userId, @PathVariable Long orderId) {
+        log.info("mappingPath userId={}, orderId={}", userId, orderId);
+        return "ok";
+    }
+
+    @GetMapping(value = "/mapping-param", params = "mode=debug") // 특정 파라미터에 조건이 있을 시 호출 (And 조건)
+    public String mappingParam() {
+        log.info("mappingParam");
+        return "ok";
+    }
+
+    @GetMapping(value = "/mapping-header", headers = "mode=debug")
+    public String mappingHeader() {
+        log.info("mappingHeader");
+        return "ok";
+    }
+
+    @PostMapping(value = "/mapping-consume", consumes = "application/json")
+    public String mappingConsumes() {
+        log.info("mappingConsumes");
+        return "ok";
+    }
+
+    @PostMapping(value = "/mapping-produce", produces = "text/html")
+    public String mappingProduces() {
+        log.info("mappingProduces");
+        return "ok";
+    }
+}
