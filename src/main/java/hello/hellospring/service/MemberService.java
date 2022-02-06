672b585020b8939a2a9f9f6dde0b057688228271
sig2nya <starwalk7058@naver.com>
2022-02-06 17:16:24 +0900

    gitignore

--- src/main/java/hello/hellospring/service/MemberService.java
+++ src/main/java/hello/hellospring/service/MemberService.java
@@ -13,10 +13,28 @@ import java.util.Optional;
 
 @Service
 public class MemberService {
-    private final MemberRepository memberRepository;
+    private final MemberRepository memberRepository = new MemoryMemberRepository();
 
-    @Autowired // 생성자에 @Autowired를 통하여 DI
+    public Long join(Member member){
+        validateDuplicateMember(member);
+        memberRepository.save(member);
+        return member.getId();
+    }
+
+    private void validateDuplicateMember(Member member){
+        memberRepository.findByName(member.getName()).ifPresent(m -> { throw new IllegalStateException("이미 존재하는 회원입니다."); });
+    }
+
+    public List<Member> findMembers(){
+        return memberRepository.findAll();
+    }
+
+    public Optional<Member> findOne(Long memberId){
+        return memberRepository.findById(memberId);
+    }
+
+    /*@Autowired // 생성자에 @Autowired를 통하여 DI
     public MemberService(MemberRepository memberRepository){
         this.memberRepository = memberRepository;
-    }
+    }*/
 }
