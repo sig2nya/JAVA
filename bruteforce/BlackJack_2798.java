e761d5980445399893539b4c1756ed7992d5c62b
sig2nya <starwalk7058@naver.com>
2022-02-17 19:12:36 +0900

    merge

--- bruteforce/BlackJack_2798.java
+++ bruteforce/BlackJack_2798.java
@@ -3,7 +3,6 @@ package bruteforce;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Scanner;
 import java.util.StringTokenizer;
 
 public class BlackJack_2798 {
