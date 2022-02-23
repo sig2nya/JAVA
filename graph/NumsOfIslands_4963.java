43de9cdc8dc833932471419b38119ba8e0d521dc
sig2nya <starwalk7058@naver.com>
2022-02-23 20:08:23 +0900

    some problem 4963

---
+++ graph/NumsOfIslands_4963.java
@@ -0,0 +1,52 @@
+package graph;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class NumsOfIslands_4963 {
+    public static int arr[][];
+    public static boolean check[][];
+    public static int dx[] = {1, -1, 0, 0};
+    public static int dy[] = {0, 0, 1, -1};
+    public static int a, b;
+    public static int count = 0;
+
+    public static void DFS(int x, int y){
+        check[x][y] = true;
+        for(int i = 0; i < 4; i++){
+            int cx = x + dx[i];
+            int cy = y + dy[i];
+            if(cx >= 0 && cy >= 0 && cx < a && cy < b){
+                if(arr[cx][cy] == 1 && !check[cx][cy]) {
+                    count++;
+                    DFS(cx, cy);
+                }
+            }
+        }
+    }
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        while(true){
+            StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+            b = Integer.parseInt(st.nextToken()); a = Integer.parseInt(st.nextToken());
+            if(a == 0 && b == 0) break;
+            arr = new int[a][b]; check = new boolean[a][b];
+            for(int i = 0; i < a; i++){
+                st = new StringTokenizer(br.readLine(), " ");
+                for(int j = 0; j < b; j++){
+                    arr[i][j] = Integer.parseInt(st.nextToken());
+                }
+            }
+            for(int i = 0; i < a; i++){
+                for(int j = 0; j < b; j++){
+                        DFS(i, j);
+                }
+            }
+            System.out.println(count);
+            count = 0;
+        }
+    }
+}
