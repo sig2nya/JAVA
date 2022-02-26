d0b943c44f3ac447cea9273ca8bfb0d1defd166f
sig2nya <starwalk7058@naver.com>
2022-02-26 15:31:27 +0900

    solved

--- programmers/크레인_인형뽑기_게임.java
+++ programmers/크레인_인형뽑기_게임.java
@@ -3,24 +3,8 @@ package programmers;
 import java.util.Stack;
 
 public class 크레인_인형뽑기_게임 {
-    /*public int solution(int board[][], int moves[]){
-
-    }*/
-
-    public static void main(String[] args) {
-        Stack stack = new Stack();
-        int board[][] = {
-                        {0,0,0,0,0},
-                        {0,0,1,0,3},
-                        {0,2,5,0,1},
-                        {4,2,4,4,2},
-                        {3,5,1,3,1}
-        };
-
-        int moves[] = {1,5,3,5,1,2,1,4};
-        // [j][moves[i] - 1]
-        // 4 3 1 1 3 2 * 4
-        int cnt = 0;
+    public static int solution(int board[][], int moves[]){
+        int cnt = 0; Stack stack = new Stack();
         for(int i = 0; i < moves.length; i++){
             for(int j = 0; j < board.length; j++){
                 if(board[j][moves[i] - 1] == 0 && board[board.length - 1][moves[i] - 1] != 0) continue;
@@ -33,9 +17,20 @@ public class 크레인_인형뽑기_게임 {
                 }
                 break;
             }
-            // System.out.println(stack.peek());
         }
+        return cnt;
+    }
 
-        System.out.println(cnt);
+    public static void main(String[] args) {
+        Stack stack = new Stack();
+        int board[][] = {
+                        {0,0,0,0,0},
+                        {0,0,1,0,3},
+                        {0,2,5,0,1},
+                        {4,2,4,4,2},
+                        {3,5,1,3,1}
+        };
+        int moves[] = {1,5,3,5,1,2,1,4};
+        System.out.println(solution(board, moves));
     }
 }
