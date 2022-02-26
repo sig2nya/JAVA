0e4159808df72b2771e0d9db1cff1b3c40a3f4f1
sig2nya <starwalk7058@naver.com>
2022-02-26 15:22:43 +0900

    keep

--- programmers/크레인_인형뽑기_게임.java
+++ programmers/크레인_인형뽑기_게임.java
@@ -22,21 +22,17 @@ public class 크레인_인형뽑기_게임 {
         // 4 3 1 1 3 2 * 3
         int cnt = 0;
         for(int i = 0; i < moves.length; i++){
-            for(int j = 0; j < 5; j++){
-                if(board[j][moves[i] - 1] == 0) continue;
+            for(int j = 0; j < board.length; j++){
+                if(board[j][moves[i] - 1] == 0 && board[board.length - 1][moves[i] - 1] != 0) continue;
                 stack.push(board[j][moves[i] - 1]);
                 board[j][moves[i] - 1] = 0;
+                if((int)stack.peek() == 0) stack.pop();
                 break;
             }
-            System.out.println(stack.peek());
+            // System.out.println(stack.peek());
         }
-        System.out.println(cnt);
-
-        for(int i = 0; i < board.length; i++){
-            for(int j = 0; j < board.length; j++){
-                System.out.print(board[i][j]);
-            }
-            System.out.println();
+        while(!stack.isEmpty()){
+            System.out.println(stack.pop());
         }
     }
 }
