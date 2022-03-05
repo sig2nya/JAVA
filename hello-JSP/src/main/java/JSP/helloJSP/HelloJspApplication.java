0a5c4ffa9722e57a10be5caf824537f00125c437
sig2nya <starwalk7058@naver.com>
2022-03-05 20:25:39 +0900

    jsp gogo

---
+++ hello-JSP/src/main/java/JSP/helloJSP/HelloJspApplication.java
@@ -0,0 +1,13 @@
+package JSP.helloJSP;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelloJspApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HelloJspApplication.class, args);
+	}
+
+}
---
+++ hello-JSP/src/test/java/JSP/helloJSP/HelloJspApplicationTests.java
@@ -0,0 +1,13 @@
+package JSP.helloJSP;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HelloJspApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
