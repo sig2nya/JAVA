3f8a0a7f4f7b71aa2920eeb376103a9142abe440
sig2nya <starwalk7058@naver.com>
2022-03-08 15:07:42 +0900

    network comute

--- Network/src/network/DemoServer.java
+++ Network/src/network/DemoServer.java
@@ -1,20 +1,34 @@
 package network;
 
+import java.io.DataOutputStream;
 import java.io.IOException;
+import java.io.OutputStream;
 import java.net.ServerSocket;
 import java.net.Socket;
 
 public class DemoServer {
     public static void main(String[] args) throws IOException {
         int port = 5050;
+        int number = Integer.parseInt(args[0]);
+        String str = new String(args[1]);
         ServerSocket ssk = new ServerSocket(port); // ServerSocket 설정
 
         System.out.println("접속 대기중...");
+
 
         while(true){
             Socket socket = ssk.accept();
             System.out.println("사용자 접속 완료.");
             System.out.println("Client IP : " + socket.getInetAddress());
+
+            OutputStream ous = socket.getOutputStream();
+            DataOutputStream dous = new DataOutputStream(ous);
+
+            dous.writeUTF(str);
+            dous.writeInt(number);
+            dous.flush();
+            dous.close();
+            socket.close();
         }
     }
 }
