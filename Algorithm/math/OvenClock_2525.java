a4abdfc9b08da0daf01e2c00ace81c320fac723e
sig2nya <starwalk7058@naver.com>
2022-03-16 18:50:35 +0900

    Algo

---
+++ Algorithm/math/OvenClock_2525.java
@@ -0,0 +1,20 @@
+package math;
+
+
+import java.util.Scanner;
+
+public class OvenClock_2525 {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        int h = sc.nextInt(); int m = sc.nextInt();
+        int C = sc.nextInt();
+        h += C / 60;
+        m += C % 60;
+        if(h >= 24) h = h - 24;
+        if(m >= 60) {
+            h++; if(h == 24) h = 0;
+            m %= 60;
+        }
+        System.out.println(h + " " + m);
+    }
+}
--- Network/src/main/java/socket/DemoServer.java
+++ Network/src/main/java/socket/DemoServer.java
@@ -9,15 +9,14 @@ import java.net.Socket;
 public class DemoServer {
     public static void main(String[] args) throws IOException {
         int port = 5050;
-        int number = Integer.parseInt(args[0]);
-        String str = new String(args[1]);
+        int number = Integer.parseInt(args[0]); // 프로그램 시작 시, 받은 매개변수.
+        String str = args[1];
         ServerSocket ssk = new ServerSocket(port); // ServerSocket 설정
 
         System.out.println("접속 대기중...");
 
-
         while(true){
-            Socket socket = ssk.accept();
+            Socket socket = ssk.accept(); // accept가 호출되면 서버는 클라이언트가 해당 포트로 연결할 때까지 무한정 대기한다.
             System.out.println("사용자 접속 완료.");
             System.out.println("Client IP : " + socket.getInetAddress());
 
