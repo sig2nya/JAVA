8494a38c6ae914b7814d4d076a473b4f384cc347
sig2nya <starwalk7058@naver.com>
2022-04-02 16:22:21 +0900

    21964

---
+++ Algorithm/string/SunlinSong_21964.java
@@ -0,0 +1,16 @@
+package string;
+
+import java.util.Scanner;
+
+public class SunlinSong_21964 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String result = "";
+        int a = Integer.parseInt(sc.nextLine());
+        String last = sc.nextLine();
+        for(int i = a - 5; i < a; i++){
+            result += last.charAt(i);
+        }
+        System.out.println(result);
+    }
+}
---
+++ Algorithm/string/Warning_3029.java
@@ -0,0 +1,12 @@
+package string;
+
+import java.util.Scanner;
+
+public class Warning_3029 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String a[]  = sc.nextLine().split(":");
+        String b[] = sc.nextLine().split(":");
+
+    }
+}
