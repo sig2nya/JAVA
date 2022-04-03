9008c3ed83035629085f939e99542c1310504cb1
sig2nya <starwalk7058@naver.com>
2022-04-03 14:20:46 +0900

    99% failed 3029

--- Algorithm/string/Warning_3029.java
+++ Algorithm/string/Warning_3029.java
@@ -7,6 +7,24 @@ public class Warning_3029 {
         Scanner sc = new Scanner(System.in);
         String a[]  = sc.nextLine().split(":");
         String b[] = sc.nextLine().split(":");
+        int h = Integer.parseInt(b[0]) - Integer.parseInt(a[0]);
+        int m = Integer.parseInt(b[1]) - Integer.parseInt(a[1]);
+        int s = Integer.parseInt(b[2]) - Integer.parseInt(a[2]);
 
+        if(s < 0){
+            s += 60;
+            m--;
+        }
+
+        if(m < 0){
+            m += 60;
+            h--;
+        }
+
+        if(h < 0){
+            h += 24;
+        }
+
+        System.out.println(String.format("%02d", h) + ":" + String.format("%02d", m) + ":" + String.format("%02d", s));
     }
 }
