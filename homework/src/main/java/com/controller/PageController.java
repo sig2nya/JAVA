f9854a5fbcac77f93c80882461ff438e8ce9174e
sig2nya <starwalk7058@naver.com>
2022-05-04 23:02:37 +0900

    .

--- homework/src/main/java/com/controller/PageController.java
+++
@@ -1,2 +0,0 @@
-package com.controller;public class PageController {
-}
--- homework/src/main/java/com/respository/ResourcesDAO.java
+++ homework/src/main/java/com/respository/ResourcesDAO.java
@@ -1,2 +1,29 @@
-package com.respository;public class ResourcesDAO {
+package com.respository;
+
+public class ResourcesDAO {
+    private int Cpu, Memory, Disk;
+
+    public int getMemory() {
+        return Memory;
+    }
+
+    public void setMemory(int memory) {
+        Memory = memory;
+    }
+
+    public int getDisk() {
+        return Disk;
+    }
+
+    public void setDisk(int disk) {
+        Disk = disk;
+    }
+
+    public int getCpu() {
+        return Cpu;
+    }
+
+    public void setCpu(int cpu) {
+        Cpu = cpu;
+    }
 }
