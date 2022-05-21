0564dc200c4f5a3b41d63b4f9a208d2fcc18c29b
HongMinSik <starwalk7058@naver.com>
2022-05-21 14:26:15 +0900

    '~'

---
+++ Algorithm/문자열/대소문자바꾸기_2744.java
@@ -0,0 +1,11 @@
+package 문자열;
+
+import java.util.Scanner;
+
+public class 대소문자바꾸기_2744 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String str = sc.nextLine();
+        
+    }
+}
