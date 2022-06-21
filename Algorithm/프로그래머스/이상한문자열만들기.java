23226d7127d5e2c6fbae41cb837da36690705c72
sig2nya <starwalk7058@naver.com>
2022-06-21 22:22:53 +0900

    rm

---
+++ Algorithm/프로그래머스/이상한문자열만들기.java
@@ -0,0 +1,27 @@
+package 프로그래머스;
+
+public class 이상한문자열만들기 {
+    public static String solution(String s){
+        String answer = "";
+        int flag = 0;
+        for(int i = 0; i < s.length(); i++){
+            if(Character.isWhitespace(s.charAt(i))) {
+                answer += " ";
+                flag++;
+                continue;
+            }
+            if(flag % 2 == 0) {
+                answer += Character.toUpperCase(s.charAt(i));
+                flag++;
+            } else {
+                answer += Character.toLowerCase(s.charAt(i));
+                flag++;
+            }
+        }
+        return answer;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(solution("try hello world"));
+    }
+}
