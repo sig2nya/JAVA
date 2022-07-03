dda4e6a65bcff8ea3b7cdbc0f79f6762af12088b
sig2nya <starwalk7058@naver.com>
2022-07-03 18:13:18 +0900

    Roles and Auth

---
+++ SpringSecurity/src/main/java/com/example/demo/security/ApplicationSecurityConfig.java
@@ -0,0 +1,55 @@
+package com.example.demo.security;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+
+@Configuration
+@EnableWebSecurity
+public class ApplicationSecurityConfig extends WebSecurityConfigurerAdapter {
+    private final PasswordEncoder passwordEncoder;
+
+    @Autowired
+    public ApplicationSecurityConfig(PasswordEncoder passwordEncoder) {
+        this.passwordEncoder = passwordEncoder;
+    }
+
+
+    // WebSecurityConfigurerAdapter는 Depreciated 되었다.
+    // Spring Security는 5.7.0-M2부터 구성 요소 기반 보안 설정으로 변경되었음.
+    // 결론은 WebSecurityConfigurerAdapter 상속 후, configure 메소드를 오버라이딩하여 설정 방식에서 SecurityFilterChain을 빈으로 등록하는 방식 권장.
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+                .authorizeRequests()
+                .antMatchers("/", "index", "/css/*", "/js/*")
+                // antMatchers를 통해서 요청 리소스에 대해 특별한 Auth가 필요 없이 접근 가능하다.
+                .permitAll()
+                .anyRequest()
+                .authenticated()
+                .and()
+                .httpBasic();
+    }
+
+    @Override
+    @Bean
+    protected UserDetailsService userDetailsService() { // User가 DB Server에 접근하기 위해 서술하는 Service
+        UserDetails annaSmithUser = User.builder()
+                .username("annasmith")
+                .password(passwordEncoder.encode("password")) // Password must be encoded로 인하여, PasswordConfig를 따로 해주어야한다.
+                .roles("STUDENT") // ROLE_STUDENT
+                .build();
+
+        return new InMemoryUserDetailsManager(
+                annaSmithUser
+        );
+    }
+}
