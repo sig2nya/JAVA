9fd4df525d0b48a924c25e0a121e59f7d0ffee29
sig2nya <starwalk7058@naver.com>
2022-08-08 20:48:51 +0900

    MVC2

--- Spring/demo-spring-security-form/src/main/java/com/example/demospringsecurityform/form/SampleService.java
+++ Spring/demo-spring-security-form/src/main/java/com/example/demospringsecurityform/form/SampleService.java
@@ -5,6 +5,7 @@ import com.example.demospringsecurityform.account.AccountContext;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Service;
 
 import java.util.Collection;
@@ -20,7 +21,11 @@ public class SampleService {
         boolean authenticated = authentication.isAuthenticated();*/
         // Thread Local을 이용하여 공유 및 참조
 
-        Account account = AccountContext.getAccount();
-        System.out.println(account.getUsername());
+        /*Account account = AccountContext.getAccount();
+        System.out.println(account.getUsername());*/
+
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        UserDetails userDetails = (UserDetails) authentication.getPrincipal();
+        System.out.println(userDetails.getUsername());
     }
 }
