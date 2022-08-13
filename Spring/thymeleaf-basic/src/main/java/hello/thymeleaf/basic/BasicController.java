2d9398114c6af6440e137be7083545dd9f12cdc7
sig2nya <starwalk7058@naver.com>
2022-08-13 11:27:38 +0900

    comments

--- Spring/thymeleaf-basic/src/main/java/hello/thymeleaf/basic/BasicController.java
+++ Spring/thymeleaf-basic/src/main/java/hello/thymeleaf/basic/BasicController.java
@@ -88,6 +88,35 @@ public class BasicController {
         return "basic/operation";
     }
 
+    @GetMapping("/attribute")
+    public String attribute(){
+        return "basic/attribute";
+    }
+
+    @GetMapping("/each")
+    public String each(Model model){
+        addUsers(model);
+        return "basic/each";
+    }
+
+    @GetMapping("/condition")
+    public String condition(Model model){
+        addUsers(model);
+        return "basic/condition";
+    }
+
+    @GetMapping("/comments")
+    public String comments(Model model){
+        model.addAttribute("data", "Spring!");
+        return "basic/comments";
+    }
+
+    @GetMapping("/block")
+    public String block(Model model){
+        addUsers(model);
+        return "basic/block";
+    }
+
     @Data
     static class User{
         private String username;
@@ -97,5 +126,15 @@ public class BasicController {
             this.username = username;
             this.age = age;
         }
+    }
+
+    private void addUsers(Model model){
+        List<User> list = new ArrayList<>();
+        list.add(new User("userA", 10));
+        list.add(new User("userB", 20));
+        list.add(new User("userC", 30));
+
+        model.addAttribute("users", list);
+
     }
 }
