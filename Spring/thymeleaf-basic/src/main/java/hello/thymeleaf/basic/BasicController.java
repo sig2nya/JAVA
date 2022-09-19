9f549933f4dd08b518793d8208969ef8d3ec0db7
sig2nya <starwalk7058@naver.com>
2022-09-19 21:30:05 +0900

    'template_fragment'

--- Spring/thymeleaf-basic/src/main/java/hello/thymeleaf/basic/BasicController.java
+++ Spring/thymeleaf-basic/src/main/java/hello/thymeleaf/basic/BasicController.java
@@ -117,7 +117,12 @@ public class BasicController {
         return "basic/block";
     }
 
-
+    @GetMapping("/javascript")
+    public String javascript(Model model){
+        model.addAttribute("user", new User("userA", 10));
+        addUsers(model);
+        return "basic/javascript";
+    }
 
     @Data
     static class User{
