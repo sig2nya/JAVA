576745f86555361c5cdb9430311903b020799f72
sig2nya <starwalk7058@naver.com>
2022-10-23 17:00:48 +0900

    'check_radio_box'

--- Spring/MVC2/form/src/main/java/hello/itemservice/web/form/FormItemController.java
+++ Spring/MVC2/form/src/main/java/hello/itemservice/web/form/FormItemController.java
@@ -2,6 +2,7 @@ package hello.itemservice.web.form;
 
 import hello.itemservice.domain.item.Item;
 import hello.itemservice.domain.item.ItemRepository;
+import hello.itemservice.domain.item.ItemType;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
@@ -9,24 +10,26 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
+import java.util.LinkedHashMap;
 import java.util.List;
+import java.util.Map;
 
 @Slf4j
-@Controller
-@RequestMapping("/form/items")
-@RequiredArgsConstructor
-public class FormItemController {
+    @Controller
+    @RequestMapping("/form/items")
+    @RequiredArgsConstructor
+    public class FormItemController {
 
-    private final ItemRepository itemRepository;
+        private final ItemRepository itemRepository;
 
-    @GetMapping
-    public String items(Model model) {
-        List<Item> items = itemRepository.findAll();
-        model.addAttribute("items", items);
-        return "form/items";
-    }
+        @GetMapping
+        public String items(Model model) {
+            List<Item> items = itemRepository.findAll();
+            model.addAttribute("items", items);
+            return "form/items";
+        }
 
-    @GetMapping("/{itemId}")
+        @GetMapping("/{itemId}")
     public String item(@PathVariable long itemId, Model model) {
         Item item = itemRepository.findById(itemId);
         model.addAttribute("item", item);
@@ -42,6 +45,7 @@ public class FormItemController {
     @PostMapping("/add")
     public String addItem(@ModelAttribute Item item, RedirectAttributes redirectAttributes) {
         log.info("item.open = {}", item.getOpen());
+        log.info("item.regions = {}", item.getRegions());
         Item savedItem = itemRepository.save(item);
         redirectAttributes.addAttribute("itemId", savedItem.getId());
         redirectAttributes.addAttribute("status", true);
@@ -61,5 +65,18 @@ public class FormItemController {
         return "redirect:/form/items/{itemId}";
     }
 
+    @ModelAttribute("regions")
+    public Map<String, String> regions(){
+        Map<String, String> regions = new LinkedHashMap<>();
+        regions.put("SEOUL", "서울");
+        regions.put("BUSAN", "부산");
+        regions.put("JEJU", "제주");
+        return regions;
+    }
+
+    @ModelAttribute("itemTypes")
+    public ItemType[] itemTypes(){
+            return ItemType.values();
+    }
 }
 
