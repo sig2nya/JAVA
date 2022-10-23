62eb8509d738ed8aa7a1ba6c8742481993d9ac5c
sig2nya <starwalk7058@naver.com>
2022-10-23 20:36:21 +0900

    'servlet'

--- Spring/MVC1/servlet/src/main/java/hello/servlet/basic/HelloServlet.java
+++ Spring/MVC1/servlet/src/main/java/hello/servlet/basic/HelloServlet.java
@@ -7,18 +7,19 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
-@WebServlet(name = "helloServlet", urlPatterns = "/hello")
+@WebServlet(name = "helloservlet", urlPatterns = "/hello")
 public class HelloServlet extends HttpServlet {
+    @Override
     protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        System.out.println("Hello, Servlet");
-        System.out.println(request);
-        System.out.println(response);
+        System.out.println("HelloServlet.service");
+        System.out.println("request = " + request);
+        System.out.println("response = " + response);
 
         String username = request.getParameter("username");
-        System.out.println(username);
+        System.out.println("username = " + username);
 
         response.setContentType("text/plain");
         response.setCharacterEncoding("utf-8");
-        response.getWriter().write("Hello, " + username);
+        response.getWriter().write("hello " + username);
     }
 }
