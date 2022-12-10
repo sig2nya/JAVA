ef05aa7d48e7bc2d484f47822b51464cd149e4eb
sig2nya <starwalk7058@naver.com>
2022-12-10 22:09:25 +0900

    'error_fixed'

--- Spring/MVC2/form/src/main/java/hello/itemservice/web/form/FormItemController.java
+++ Spring/MVC2/form/src/main/java/hello/itemservice/web/form/FormItemController.java
@@ -9,7 +9,9 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
+import java.util.LinkedHashMap;
 import java.util.List;
+import java.util.Map;
 
 @Controller
 @RequestMapping("/form/items")
@@ -42,6 +44,7 @@ public class FormItemController {
     @PostMapping("/add")
     public String addItem(@ModelAttribute Item item, RedirectAttributes redirectAttributes) {
         log.info("item.open={}", item.getOpen());
+        log.info("item.regions={}", item.getRegions());
         Item savedItem = itemRepository.save(item);
         redirectAttributes.addAttribute("itemId", savedItem.getId());
         redirectAttributes.addAttribute("status", true);
@@ -59,6 +62,15 @@ public class FormItemController {
     public String edit(@PathVariable Long itemId, @ModelAttribute Item item) {
         itemRepository.update(itemId, item);
         return "redirect:/form/items/{itemId}";
+    }
+
+    @ModelAttribute("regions") // 등록 폼, 상세화면, 수정 폼에서 모두 서울, 부산, 제주라는 check박스를 반복 렌더링 수행해야 함
+    public Map<String, String> regions(){ // 각 controller에서 model.addAttribute를 사용해야 하며, 반복이 발생
+        Map<String, String> regions = new LinkedHashMap<>(); // @ModelAttribute는 Controller의 별도 메서드에 적용 가능
+        regions.put("SEOUL", "서울"); // 해당 애노테이션을 적용하면, 컨트롤러 요청시, regions에서 반환 값이 자동으로 model에 담긴다.
+        regions.put("BUSAN", "부산");
+        regions.put("JEJU", "제주");
+        return regions;
     }
 
 }
